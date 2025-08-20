241874bceb7a192dd1f65407476a967f766beb46
Nikita <ns_stepanov@inbox.ru>
2025-08-20 14:33:40 +0300

    Add basic interfaces

---
+++ pg.go
@@ -0,0 +1,44 @@
+package pg
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type Client interface {
+	Query(sql string, model any) Query
+	Command(sql string, model any) Command
+	Transactional(ctx context.Context, fn TxFunc) error
+
+	ToPgx() *pgxpool.Pool
+	ToDB() *sql.DB
+}
+
+type Query interface {
+	WithArgs(args ...[]*Argument) Query
+	WithArg(key string, value any) Query
+	Exec(ctx context.Context) error
+}
+
+type Command interface {
+	WithArgs(args ...[]*Argument) Command
+	WithArg(key string, value any) Command
+	Returning(dest any) Command
+	Exec(ctx context.Context) error
+}
+
+func Arg(key string, value any) *Argument {
+	return &Argument{
+		key,
+		value,
+	}
+}
+
+type Argument struct {
+	key   string
+	value any
+}
+
+type TxFunc func(ctx context.Context) error
